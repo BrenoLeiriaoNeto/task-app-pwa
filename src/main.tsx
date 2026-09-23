b05c7bbe13d81b353a72169bb7923994e97e1ad0
BrenoLeiriaoNeto <bleiriaoneto@icloud.com>
2026-09-23 15:17:08 -0300

    feat: integrate Firebase Analytics service with event tracking

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import './storage/firebase/firebaseConfig.ts'
 import MyApp from './MyApp.tsx'
 import {KonstaProvider} from "konsta/react";
 
